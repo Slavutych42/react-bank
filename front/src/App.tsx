2a51a16ec179d147d22e86f2693c327e991d5e40
Slavutych42 <128924265+Slavutych42@users.noreply.github.com>
2024-01-12 13:10:07 +0200

    q

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,112 @@
-import React from "react";
+import { React } from "react";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <AuthContext.Provider value={authContextData}>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            index
+            element={
+              <AuthRoute>
+                <WellcomePage />
+              </AuthRoute>
+            }
+          />
+          <Route
+            path="/signup"
+            element={
+              <AuthRoute>
+                <SignupPage />
+              </AuthRoute>
+            }
+          />
+          <Route
+            path="/signup-confirm"
+            element={
+              <PrivateRoute>
+                <SignupConfirmPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/signin"
+            element={
+              <AuthRoute>
+                <SigninPage />
+              </AuthRoute>
+            }
+          />
+          <Route
+            path="/recovery"
+            element={
+              <AuthRoute>
+                <RecoveryPage />
+              </AuthRoute>
+            }
+          />
+          <Route
+            path="/recovery-confirm"
+            element={
+              <AuthRoute>
+                <RecoveryConfirmPage />
+              </AuthRoute>
+            }
+          />
+          <Route
+            path="/balance"
+            element={
+              <PrivateRoute>
+                <BalancePage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/notifications"
+            element={
+              <PrivateRoute>
+                <NotificationsPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/settings"
+            element={
+              <PrivateRoute>
+                <SettingsPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/recive"
+            element={
+              <PrivateRoute>
+                <RecivePage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/send"
+            element={
+              <PrivateRoute>
+                <SendPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/transaction/:transactionId"
+            element={
+              <PrivateRoute>
+                <TransactionPage />
+              </PrivateRoute>
+            }
+          />
+          <Route path="*" Component={Error} />
+        </Routes>
+      </BrowserRouter>
+    </AuthContext.Provider>
+  );
+  
 }
 
 export default App;
